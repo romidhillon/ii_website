8eb3e6cf2e8c76c387e5dbfbac18996fb15183c9
Romi Dhillon <romidhillon_13@hotmail.co.uk>
2022-09-24 19:58:43 +0100

    updates models.py

---
+++ ii_app/choices.py
@@ -0,0 +1,45 @@
+
+status_choices = (
+        ('O','Open'),
+        ('C','Closed'),
+    )
+
+status_choices = (
+        ('O','Open'),
+        ('C','Closed'),
+    )
+
+position_choices = (
+    ('DAR', 'Data Architect'),
+    ('DA', 'Data Analyst'),
+    ('DS', 'Data Scientist'),
+    ('SA', 'Solution Architect'),
+    ('BA', 'Business Analyst'),
+    ('PM', 'Project Manager'),
+    ('PC', 'Project Coordinator'),
+    ('SME', 'Subject Matter Expert'),
+    ('PO', 'Product Owner'),
+    ('SE', 'Safety Engineer'),
+    ('TL', 'Team Lead'),
+    ('UX', 'UX Designer'),
+    ('PL', 'Project Lead'),
+)
+
+risk_impact_choices = (
+        ('L','Low'),
+        ('M','Medium'),
+        ('H','High'),
+    )
+
+risk_probability_choices = (
+        ('L','Low'),
+        ('M','Medium'),
+        ('H','High'),
+    )
+
+risk_owner_choices = (
+    ('RD', 'Romi Dhillon'),
+    ('SH', 'Serena Haak'),
+    ('TK', 'Tom Kinnear'),
+    ('JC', 'Joe Collis'),
+)
--- ii_app/models.py
+++ ii_app/models.py
@@ -1,4 +1,12 @@
+from ast import Delete
 from django.db import models
+from ii_website import settings
+from .choices import status_choices
+from .choices import position_choices
+from .choices import risk_impact_choices
+from .choices import risk_probability_choices
+from .choices import risk_owner_choices
+
 
 # Create your models here.
 # models.Model is taking in the Model blueprint from the model function within the django.db library 
@@ -10,22 +18,38 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
+    employee_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
-    position = models.CharField(max_length=200)
+    position = models.CharField(max_length=200, choices=position_choices)
     day_rate = models.FloatField()
     cf_number = models.CharField(max_length=200)
     project_name = models.CharField(max_length=200)
-    item_image = models.CharField(max_length=500, default="https://eu.evocdn.io/dealer/1065/catalog/product/images/cs_1612369426.png")
+    hours_booked = models.FloatField
     image = models.ImageField(upload_to='images/')
     
 class Risk(models.Model):
 
     risk_description = models.CharField(max_length = 500)
-    risk_impact = models.IntegerField()
-    risk_probability = models.IntegerField()
+    risk_impact = models.CharField(max_length = 50, choices= risk_impact_choices)
+    risk_probability = models.CharField(max_length = 50, choices = risk_probability_choices)
     risk_mitigation = models.CharField(max_length = 500)
-    risk_owner = models.CharField(max_length = 200)
-    risk_status = models.CharField(max_length = 500)
+    risk_owner = models.CharField(max_length = 200, choices = risk_owner_choices)
+    risk_status = models.CharField(max_length = 500, choices= status_choices)
     date_opened = models.DateField()
 
+class Cone (models.Model):
 
+    def __str__(self):
+        return self.employee_id
+
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    cone_rate = models.FloatField()
+
+class Bookings (models.Model):
+
+    def __str__(self):
+        return self.employee_id
+
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    week_ending_date = models.DateField()
+    hours_booked = models.FloatField()
